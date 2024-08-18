0d42109ab53ed2b77fe622fa5c133b5d5c1ffbf7
Naimul02 <naimulislum39@gmail.com>
2024-08-18 01:17:22 -0700

    commit no 14

--- src/Pages/Products/DropdownMenu.jsx
+++ src/Pages/Products/DropdownMenu.jsx
@@ -4,7 +4,7 @@ import { IoIosArrowDown } from 'react-icons/io';
 const DropdownMenu = ({handleLowToHigh , handleHighToLow , handleDate}) => {
     return (
           
-          <div className=" mx-[70px] flex justify-end">
+          <div className="lg:mx-[70px] mx-20 flex  mb-[140px] lg:mb-0  justify-center lg:justify-end">
           <div className="dropdown dropdown-end" >
         <div tabIndex={0} role="button" className="btn m-1 bg-white hover:bg-white w-[229px] border rounded-none">Sort Products By  <IoIosArrowDown className="text-2xl font-semibold"/></div>
     <ul tabIndex={0} className="dropdown-content menu bg-white border rounded-none z-[1] w-[229px] p-2 shadow">
